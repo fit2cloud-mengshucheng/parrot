fd0b41d0674d03195d7e4098b881378c6f37de32
mengshucheng <mengshucheng>
2023-07-29 21:06:26 +0800

    py web GUI dome

--- parrot_web.py
+++ parrot_web.py
@@ -1,1 +1,19 @@
 # TODO python web 研究
+
+import parrot_listen as pl
+
+from kivy.core.window import Window
+from kivy.app import App
+from kivy.uix.button import Button
+
+Window.size = (400, 300)
+
+
+class MyApp(App):
+
+    def build(self):
+        return Button(text='Click me', on_press=lambda e: pl.listen_func())
+
+
+if __name__ == '__main__':
+    MyApp().run()
